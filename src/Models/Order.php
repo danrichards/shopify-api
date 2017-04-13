c2679b200edb0d537c36fbda9d92eddfbaa3f5aa
Dan Richards <dan@shineon.com>
2017-04-13 15:48:17 -0400

    Constants and a strict mode.

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -174,6 +174,50 @@ class Order extends AbstractModel
 
     use OwnsMetafields, Taggable;
 
+    // Financial statuses from Shopify
+    const FINANCIAL_STATUS_AUTHORIZED = 'authorized';
+    const FINANCIAL_STATUS_PAID = 'paid';
+    const FINANCIAL_STATUS_PARTIALLY_PAID = 'partially_paid';
+    const FINANCIAL_STATUS_PARTIALLY_REFUNDED = 'partially_refunded';
+    const FINANCIAL_STATUS_PENDING= 'pending';
+    const FINANCIAL_STATUS_REFUNDED = 'refunded';
+    const FINANCIAL_STATUS_VOIDED = 'voided';
+
+    /** @var array $financial_statuses */
+    public static $financial_statuses = [
+        self::FINANCIAL_STATUS_AUTHORIZED,
+        self::FINANCIAL_STATUS_PAID,
+        self::FINANCIAL_STATUS_PARTIALLY_PAID,
+        self::FINANCIAL_STATUS_PARTIALLY_REFUNDED,
+        self::FINANCIAL_STATUS_PENDING,
+        self::FINANCIAL_STATUS_REFUNDED,
+        self::FINANCIAL_STATUS_VOIDED
+    ];
+
+    // Fulfillment statuses from Shopify
+    const FULFILLMENT_STATUS_FILLED = 'fulfilled';
+    const FULFILLMENT_STATUS_PARTIAL = 'partial';
+    const FULFILLMENT_STATUS_UNFILLED = null;
+
+    /** @var array $fulfillment_statuses */
+    public static $fulfillment_statuses = [
+        self::FULFILLMENT_STATUS_FILLED,
+        self::FULFILLMENT_STATUS_PARTIAL,
+        self::FULFILLMENT_STATUS_UNFILLED
+    ];
+
+    // Risk recommendations from Shopify
+    const RISK_RECOMMENDATION_LOW = 'accept';
+    const RISK_RECOMMENDATION_MEDIUM = 'investigate';
+    const RISK_RECOMMENDATION_HIGH = 'cancel';
+
+    /** @var array $risk_statuses */
+    public static $risk_statuses = [
+        self::RISK_RECOMMENDATION_LOW,
+        self::RISK_RECOMMENDATION_MEDIUM,
+        self::RISK_RECOMMENDATION_HIGH
+    ];
+
     /** @var string $api_name */
     protected static $api_name = 'order';
 
