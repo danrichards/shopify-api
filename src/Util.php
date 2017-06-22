2f02273289838a8341e1eb8c9a69071382f9bd74
Dan Richards <dan@shineon.com>
2017-06-22 09:00:37 -0400

    Combined classes.

--- src/Util.php
+++ src/Util.php
@@ -2,6 +2,8 @@
 
 namespace ShopifyApi;
 
+use Guzzle\Http\Message\Response;
+
 /**
  * Class Util
  */
@@ -80,6 +82,23 @@ class Util
         );
 
         return $hmac == $calculated_hmac;
+    }
+
+    /**
+     * @param Response $response
+     * @return \Guzzle\Http\EntityBodyInterface|mixed|string
+     */
+    public static function getContent(Response $response)
+    {
+        $body = $response->getBody(true);
+
+        $content = json_decode($body, true);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            return $body;
+        }
+
+        return $content;
     }
 
 }
